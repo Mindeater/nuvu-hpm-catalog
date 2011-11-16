4b670eb028a58e370aa60dfbe28d2665165f10cd
Ashley McCoy <ashey@mindeater.com>
2011-11-16 16:16:39 +1100

    removes views when drawWithMechanisms is called, Handles the differences in Arteor 770 data when mechanism names have AR in front of them, scales mechanism images to fit screen size

--- Catalog2/MechanismView.m
+++ Catalog2/MechanismView.m
@@ -15,42 +15,63 @@
 
 -(void)drawWithMechanisms:(NSArray *)mechanisms
 {
-
-    //self.currentObject = obj;
-    // NSLog(@"Mechanism  Loaded %@",self.parentProduct);
+    // remove any views that already exist
+    for (UIView *view in [self subviews]) { [view removeFromSuperview]; }
+    
+    self.backgroundColor = [UIColor clearColor];
+    NSLog(@"Mechanism  Loaded %@",mechanisms);
     
     CGRect screenRect = [[UIScreen mainScreen] bounds];
     CGFloat screenWidth = screenRect.size.width;
-    //CGFloat screenHeight = screenRect.size.height;
+    CGFloat screenHeight = screenRect.size.height;
     
     //Product Name
-    UILabel *myLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, screenWidth - 40, 100)];      
+    UILabel *myLabel = [[UILabel alloc] initWithFrame:CGRectMake(20,screenHeight - 150, screenWidth - 40, 100)];      
     myLabel.text = self.parentProduct;
     myLabel.textColor = [UIColor redColor];
     [self addSubview:myLabel];
     [myLabel release];
     
     //Picture/s
+    NSLog(@"****  \n\n\n\n\n");
     
+    // note:
+    //arteor 770 has AR in front of the part name which doesn't match the image
     for(NSManagedObject *mech in mechanisms){
         
-        NSLog(@" ++ id:%@ count:%@",[mech valueForKey:@"id"],[mech valueForKey:@"count"] );
+        NSLog(@"%@\n\n ++ id:%@ count:%@ name: %@",self.parentProduct,[mech valueForKey:@"id"],[mech valueForKey:@"count"],[mech valueForKey:@"name"] );
         NSString *img;
-        if([[mech valueForKey:@"count"] intValue] == 1){
-            // load the image based on the id
-            img = [mech valueForKey:@"id"];
+        if([[mech valueForKey:@"count"] isEqualToNumber:[NSNumber numberWithInt:1]]){
+            // load the image based on the id screen the Arteor 770 fields
+            if([[mech valueForKey:@"name"] isEqualToString:@"Mech 2 Part #"]){
+                img = [[mech valueForKey:@"id"] 
+                   stringByReplacingOccurrencesOfString:@"AR" withString:@""];
+            }else{
+                img = [mech valueForKey:@"id"];
+            }
         }else{
             // build the image name
-            img = [NSString stringWithFormat:@"%d-x-%@",
+            
+            
+            img = [NSString stringWithFormat:@"%@-x-%@",
                              [mech valueForKey:@"count"],
-                             [mech valueForKey:@"id"]];
+                             [[mech valueForKey:@"id"]
+                              stringByReplacingOccurrencesOfString:@"AR" withString:@""]];
         }
         //
-        NSString *imageName = [[NSBundle mainBundle] pathForResource:img ofType:@"png"];
+        NSLog(@"  - image name is %@",[img stringByReplacingOccurrencesOfString:@"/" withString:@"-"]);
+        NSString *imageName = [[NSBundle mainBundle] 
+                               pathForResource:[img stringByReplacingOccurrencesOfString:@"/" withString:@"-"]
+                               ofType:@"png"];
         
         UIImage *image = [UIImage imageWithContentsOfFile:imageName];
         UIImageView *nextImage = [[UIImageView alloc] initWithImage:image];
-        NSLog(@" mechanism added %@ =\n%@",imageName,nextImage);
+        nextImage.backgroundColor = [UIColor clearColor];
+        // calculate it's size
+        nextImage.contentMode = UIViewContentModeScaleAspectFit;
+        //int width = image.size.width 
+        nextImage.frame = CGRectMake(20, 20, screenWidth -20, screenHeight - 20);
+        //NSLog(@" mechanism picture added %@ =\n\n\n",imageName);
         [self addSubview:nextImage];
         [nextImage release];
         
