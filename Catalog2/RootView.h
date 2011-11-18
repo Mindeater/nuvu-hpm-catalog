6915408aa1086fe7aa765599b1d7389a86cc4dd2
Ashley McCoy <ashey@mindeater.com>
2011-11-18 15:09:33 +1100

    now handles six buttons and they sty in the centre of the screen independent of orientation

--- Catalog2/RootView.h
+++ Catalog2/RootView.h
@@ -10,11 +10,24 @@
 
 @interface RootView : UIViewController{
     NSManagedObjectContext *context;
+    UIButton *button1;
+    UIButton *button2;
+    UIButton *button3;
+    UIButton *button4;
+    UIButton *button5;
+    UIButton *button6;
 }
 
 @property(nonatomic,retain)NSManagedObjectContext *context;
+@property(nonatomic,retain)UIButton *button1;
+@property(nonatomic,retain)UIButton *button2;
+@property(nonatomic,retain)UIButton *button3;
+@property(nonatomic,retain)UIButton *button4;
+@property(nonatomic,retain)UIButton *button5;
+@property(nonatomic,retain)UIButton *button6;
 
 -(void)showAlert:(id)sender;
 -(void)showCatalog;
+-(void)layoutButtons:(UIInterfaceOrientation)toInterfaceOrientation;
 
 @end
--- Catalog2/RootView.m
+++ Catalog2/RootView.m
@@ -12,6 +12,7 @@
 @implementation RootView
 
 @synthesize context;
+@synthesize button1,button2,button3,button4,button5,button6;
 
 -(void)dealloc
 {
@@ -19,6 +20,7 @@
     //[self.view release];
     [super dealloc];
 }
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -53,23 +55,93 @@
     // create the main view Controller
     //allocate the view
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
-    
-    /* this works for 4.3
-     CGRect buttonFrame = CGRectMake( 10, 80, 100, 30 );
-     UIButton *button = [[UIButton alloc] initWithFrame: buttonFrame];
-     [button setTitleColor: [UIColor redColor] forState: UIControlStateNormal];
-     */
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    button.frame = CGRectMake(100, 170, 100, 60);
-    [button setTitle:@"Catalog" forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(showCatalog) forControlEvents:UIControlEventTouchUpInside];
-    
-    [self.view addSubview:button];
-    //[button release];
+    self.view.backgroundColor = [UIColor blackColor];
 
+    // main Navigation Options
+    // 1. show catalog
+    button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button1 setTitle:@"Catalog" forState:UIControlStateNormal];
+    [button1 addTarget:self action:@selector(showCatalog) forControlEvents:UIControlEventTouchUpInside];
+
+    // 2. Choose Background (from supplied)
+    button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button2 setTitle:@"Choose BG" forState:UIControlStateNormal];
+    [button2 addTarget:self action:@selector(showAlert:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // 3. Create Background (camera or library)
+    button3 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button3 setTitle:@"Create BG" forState:UIControlStateNormal];
+    [button3 addTarget:self action:@selector(showAlert:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // 4. show Orders
+    button4 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button4 setTitle:@"Orders" forState:UIControlStateNormal];
+    [button4 addTarget:self action:@selector(showAlert:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // 5. show Settings
+    button5 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button5 setTitle:@"Settings" forState:UIControlStateNormal];
+    [button5 addTarget:self action:@selector(showAlert:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // 6. show Search
+    button6 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button6 setTitle:@"Searc" forState:UIControlStateNormal];
+    [button6 addTarget:self action:@selector(showAlert:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //[[UIApplication sharedApplication] statusBarOrientation];
+    [self layoutButtons: [[UIApplication sharedApplication] statusBarOrientation]];
+    
+    [self.view addSubview:button1];
+    [self.view addSubview:button2];
+    [self.view addSubview:button3];
+    [self.view addSubview:button4];
+    [self.view addSubview:button5];
+    [self.view addSubview:button6];
    
 }
 
+-(void)layoutButtons:(UIInterfaceOrientation)toInterfaceOrientation
+{
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.width;
+    CGFloat screenHeight = screenRect.size.height;
+    if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft ||
+        toInterfaceOrientation == UIInterfaceOrientationLandscapeRight)
+    {
+        screenWidth = screenRect.size.height;
+        screenHeight = screenRect.size.width;
+    }
+
+
+    
+    float buttonWidth = 100;
+    float buttonHeight = 100;
+    float pad = 20;
+    float startX = (screenWidth - (3 * buttonWidth + pad * 2))/2;
+    float startY = (screenHeight -(2 * buttonHeight+pad))/2;
+    
+    button1.frame =  CGRectMake(startX, startY,
+                                buttonWidth, buttonHeight);
+    button2.frame =  CGRectMake(startX + buttonWidth+pad, startY,
+                                buttonWidth, buttonHeight);
+    button3.frame =  CGRectMake((startX + (buttonWidth * 2.0))+2*pad, startY,
+                                buttonWidth, buttonHeight);
+    button4.frame =  CGRectMake(startX, startY + buttonHeight+pad,
+                                buttonWidth, buttonHeight);
+    button5.frame =  CGRectMake(startX + buttonWidth +pad, startY + buttonHeight+pad,
+                                buttonWidth, buttonHeight);
+    button6.frame =  CGRectMake(startX + buttonHeight*2+2*pad, startY + buttonHeight+pad,
+                                buttonWidth, buttonHeight);
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    [self layoutButtons:toInterfaceOrientation];
+    
+}
+
+
+#pragma mark - Button call Backs
 -(void)showCatalog
 {
     NSLog(@"StartCatalog");
@@ -83,7 +155,7 @@
 
 -(void)showAlert:(id)sender 
 {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"My App" message: @"Welcome to ******. \n\nSome Message........" delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"HPM Catalog" message: @"This feature. \n\nNot Yet Available" delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
     [alert show];
     [alert release];
 }
@@ -110,12 +182,18 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.button1 = nil;
+    self.button2 = nil;
+    self.button3 = nil;
+    self.button4 = nil;
+    self.button5 = nil;
+    self.button6 = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES; //(interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
