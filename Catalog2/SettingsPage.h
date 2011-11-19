84532826d0f2578ecfd2aa2042237c2c071ab77b
Ashley McCoy <ashey@mindeater.com>
2011-11-19 15:55:17 +1100

    Basic settings page UIView

---
+++ Catalog2/SettingsPage.h
@@ -0,0 +1,13 @@
+//
+//  SettingsPage.h
+//  Catalog2
+//
+//  Created by Ashley McCoy on 18/11/11.
+//  Copyright (c) 2011 Mindeater Web Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsPage : UIViewController
+
+@end
