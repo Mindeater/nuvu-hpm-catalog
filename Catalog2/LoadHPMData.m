f89573c60a499f35ecd05bc4bd50829739a0c35f
Ashley McCoy <ashey@mindeater.com>
2011-11-24 15:07:35 +1100

    Quietened logging - tried to ensure unique entities but complexity became too much

--- Catalog2/LoadHPMData.m
+++ Catalog2/LoadHPMData.m
@@ -23,14 +23,40 @@
 
 -(NSManagedObject *)addEntityWithValues:(NSString*)entity:(NSDictionary *)keyValues
 {
-    NSLog(@" - Insertinto : %@",entity);
     NSError *error = nil;
+    // does the entity already exist ??
+    /* TOO COMPLEX !!
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    NSEntityDescription *existingEntity = [NSEntityDescription entityForName:entity
+                                              inManagedObjectContext:self.context];
+    // Mechanism uses id ad the Unique identifier - yeah :(
+    NSPredicate *predicate;
+    if([entity isEqualToString:@"Mechanism"]){
+        predicate = [NSPredicate predicateWithFormat:@"(%K == %@)",
+                     @"id",
+                     [keyValues objectForKey:@"id"]];
+    }else{
+        predicate = [NSPredicate predicateWithFormat:@"(%K == %@)",
+                              @"name",
+                              [keyValues objectForKey:@"name"]];
+    }
+    [request setPredicate:predicate];
+    
+    [request setEntity:existingEntity];
+    NSArray *result = [self.context executeFetchRequest:request error:&error];
+    [request release];
+    if([result lastObject]){
+        NSLog(@"\n\n     Existing Entity : %@\n\n\n",[keyValues objectForKey:@"name"]);
+        return [result lastObject];
+    }
+     */
+    // if not create it
     NSManagedObject *newEntity = [NSEntityDescription 
                                  insertNewObjectForEntityForName:entity 
                                  inManagedObjectContext:self.context];
     NSString *key;
     for(key in keyValues){
-        NSLog(@"Key: %@, Value %@", key, [keyValues objectForKey: key]);
+        //NSLog(@"Key: %@, Value %@", key, [keyValues objectForKey: key]);
         [newEntity setValue:[keyValues objectForKey: key] forKey:key];
     }
     
@@ -38,7 +64,7 @@
     [self.context insertObject:newEntity];
     [self.context save:&error];
     if(error){
-        NSLog(@" Error adding %@ with %@ \n Error: %@",entity,keyValues,[error localizedDescription]);
+        NSLog(@" Error adding %@ with %@ \n \nError: %@\n\n",entity,keyValues,[error localizedDescription]);
     } 
     return newEntity;
 }
@@ -76,13 +102,12 @@
          
         
         NSDictionary *pListData = [self readPlist:[brand objectAtIndex:0]];
-        NSLog(@"Started");
         // insert Brand 
         
         NSManagedObject *brandObj = [self addEntityWithValues:@"Brand":
                                          [NSDictionary dictionaryWithObject:[brand objectAtIndex:1]forKey:@"name" ]];
          
-        NSLog(@"Brand : %@",[brand objectAtIndex:1]);
+        NSLog(@"1. Brand INSERT : %@",[brand objectAtIndex:1]);
         
         NSDictionary *catInBrand = [pListData objectForKey:[brand objectAtIndex:0]];
         
@@ -92,29 +117,42 @@
         while ((catName = [enumerator nextObject])) {
             
             //insert Cat
-            NSLog(@" - Category: %@",catName);
+            
             NSManagedObject *catObj = [self addEntityWithValues:@"Category":
                                              [NSDictionary dictionaryWithObjectsAndKeys:
                                               catName,@"name",
                                               brandObj,@"brand",
                                               nil]];
-             
+             NSLog(@"2. - Category INSERT : %@",catName);
             
             // Loop products
             NSEnumerator *catEnum = [[catInBrand objectForKey:catName] keyEnumerator];
             id product;
             while((product = [catEnum nextObject])){
             
-                //insert product
+                // get the orientation Horizontal or Vertical or Both
+                NSString *orientation;
+                NSRange range = [catName rangeOfString:@"Vertical"];
+                if (range.location != NSNotFound){
+                    orientation = @"Vertical";
+                    NSRange range2 = [catName rangeOfString:@"Horizontal"];
+                    if(range2.location != NSNotFound){
+                        orientation = @"Both";
+                    }
+                }else{
+                    orientation = @"Horizontal";
+                }
+
                 
                 NSManagedObject *productObj = [self addEntityWithValues:@"Product":
                                            [NSDictionary dictionaryWithObjectsAndKeys:
-                                            product,@"name",
-                                            brandObj,@"brand",
-                                            catObj,@"category",
+                                            product,     @"name",
+                                            brandObj,    @"brand",
+                                            catObj,      @"category",
+                                            orientation, @"orientation",
                                             nil]];
                  
-                NSLog(@" - - Product : %@",product);
+                NSLog(@"3. - - Product INSERT : %@",product);
                 
                 NSArray *keys = [[catInBrand objectForKey:catName] objectForKey:product];
                 
@@ -125,7 +163,9 @@
                     NSString *value = [[[keys objectAtIndex:i] allObjects] lastObject];
                     NSString *nextHeading = [[[keys objectAtIndex:i+1] allKeys] lastObject];
                     NSString *nextValue = [[[keys objectAtIndex:i+1] allObjects] lastObject];
-                    NSLog(@"%@ ->",heading);
+                    
+                    NSLog(@"4. - - - - PART NAME : %@ ->",heading);
+                    
                     if ([heading isEqualToString:@"Mechanism 1"]
                         ||[heading isEqualToString:@"Frame"]
                         ||[heading isEqualToString:@"Mechanism 3"]
@@ -134,9 +174,9 @@
                         if([nextHeading isEqualToString:@"Trade Price"]){
                             if (![value isEqualToString:@""]) {
                                // push in the mechanism 
-                                NSLog(@" - Mech %@ price %f",value,[nextValue doubleValue]);
-                                NSLog(@" %d",[@"1" intValue]);
-                                NSLog(@"%@",heading);
+                                //NSLog(@" - Mech %@ price %f",value,[nextValue doubleValue]);
+                                //NSLog(@" %d",[@"1" intValue]);
+                               // NSLog(@"%@",heading);
                                 
                                // NSManagedObject *mechObj = 
                                 [self addEntityWithValues:@"Mechanism":
@@ -162,7 +202,7 @@
                             //NSNumber *price = [[[productParts objectForKey:[keys objectAtIndex:i+2]]allObjects]lastObject];
                             NSString *price = [[[keys objectAtIndex:i+2] allObjects] lastObject];
                             // push in the mechanism
-                            NSLog(@" - Mech %@ price %@ quan %@",value,price,nextValue);
+                            NSLog(@"5. -  - - - Mechanism %@ price %@ quan %@",value,price,nextValue);
                             
                             //NSManagedObject *mechObj = 
                             [self addEntityWithValues:@"Mechanism":
@@ -182,7 +222,7 @@
                         if([nextHeading isEqualToString:@"Trade Price"]){
                             if (![value isEqualToString:@""]) {
                                 // push in the FacePlate
-                                NSLog(@" - - Plate %@ price %@",value,nextValue);
+                                NSLog(@"6. - - - - - Plate %@ price %@",value,nextValue);
                                 
                                // NSManagedObject *plateObj = 
                                 [self addEntityWithValues:@"Faceplate":
@@ -206,19 +246,19 @@
         
     }
 }
+
 -(id)init
 {
-    NSLog(@"INIT");
     self.pListFiles = [NSArray arrayWithObjects:
-                       [NSArray arrayWithObjects:@"arteor-770.csv",    @"Arteor 770",nil],
+                       /*[NSArray arrayWithObjects:@"arteor-770.csv",    @"Arteor 770",nil],
                        [NSArray arrayWithObjects:@"arteor-rd.csv",     @"Arteor RD",nil],
                         [NSArray arrayWithObjects:@"arteor-sq.csv",     @"Arteor SQ",nil],
-                       /* [NSArray arrayWithObjects:@"bt-light-and-tech.csv", @"BT Light and Tech",nil],
+                        [NSArray arrayWithObjects:@"bt-light-and-tech.csv", @"BT Light and Tech",nil],
                         [NSArray arrayWithObjects:@"bt-living.csv",     @"BT Living",nil],
                         [NSArray arrayWithObjects:@"excel-life.csv",   @"Excel Life",nil],
                         [NSArray arrayWithObjects:@"excel.csv",        @"Excel",nil],
-                        [NSArray arrayWithObjects:@"linea.csv",        @"Linea",nil],
-                        */nil];
+                        */[NSArray arrayWithObjects:@"linea.csv",        @"Linea",nil],
+                        nil];
 
     return self;
 }
