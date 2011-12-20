39c611637c96acbb5d09d11c0cb19b67b09b3365
Ashley McCoy <ashey@mindeater.com>
2011-12-20 14:08:26 +1100

    Jeff LaMarche subclass on UIAlertView to capture user input

---
+++ Catalog2/AlertPrompt.h
@@ -0,0 +1,24 @@
+//
+//  AlertPrompt.h
+//  Catalog2
+//
+//  Created by Ashley McCoy on 20/12/11.
+//  Copyright (c) 2011 Mindeater Web Services. All rights reserved.
+//
+
+//
+//  AlertPrompt.h
+//  Prompt
+//
+//  Created by Jeff LaMarche on 2/26/09.
+
+#import <Foundation/Foundation.h>
+
+@interface AlertPrompt : UIAlertView 
+{
+    UITextField *textField;
+}
+@property (nonatomic, retain) UITextField *textField;
+@property (readonly) NSString *enteredText;
+- (id)initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle okButtonTitle:(NSString *)okButtonTitle;
+@end
