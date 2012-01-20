826e619baf5f1a08e1d0374ad4cbd025d4e51e3d
Ashley McCoy <ashey@mindeater.com>
2012-01-20 14:09:27 +1100

    handling the case where a faceplate is used without an image or part number ..

--- Catalog2/FacePlateView.m
+++ Catalog2/FacePlateView.m
@@ -42,51 +42,53 @@
     [self.view addSubview:bgMech];
     [bgMech release];
     
-    
-    /////////////////////////////////////
-    // Build the file path and image name
-    NSString *img = [[items lastObject] valueForKey:@"id"];
-    
-    //NSLog(@"\n\nCoverplate has been passed :: \n\n\n %@",items);
-  
-    NSString *dir = [NSString stringWithFormat:@"%@/Faceplate",
-           [self.brandName stringByReplacingOccurrencesOfString:@" " withString:@""]];
-    
-    NSString *imgCleaned = [NSString stringWithFormat:@"%@%@",
-                            self.orientationPrefix,
-                            [img stringByReplacingOccurrencesOfString:@"/" withString:@"-"]];
-    
-   // NSLog(@"\nCoverPlate \n\n - File NAme %@ \nwith PAth: %@\n\n\n\n",imgCleaned,dir);
-    NSString *imageName = [[NSBundle mainBundle] 
-                           pathForResource:imgCleaned
-                           ofType:@"png" 
-                           inDirectory:dir];
-    //NSLog(@"\nFacePlate added from - - - Path\n %@\n",imageName);
-    
-    ///////////////////////////////
-    // store internals
-    self.parts = [NSString stringWithFormat:@"%@",imgCleaned];
-    self.price = [[items lastObject] valueForKey:@"price"];
-    
-    /////////////////////////////////////////
-    // Grab the image off disk and load it up
+    NSLog(@" Part view Draw %i",[items count]);
+    if([items count] >1){
+        NSLog(@" looking for coverplate to draw");
+        /////////////////////////////////////
+        // Build the file path and image name
+        NSString *img = [[items lastObject] valueForKey:@"id"];
+        
+        //NSLog(@"\n\nCoverplate has been passed :: \n\n\n %@",items);
+      
+        NSString *dir = [NSString stringWithFormat:@"%@/Faceplate",
+               [self.brandName stringByReplacingOccurrencesOfString:@" " withString:@""]];
+        
+        NSString *imgCleaned = [NSString stringWithFormat:@"%@%@",
+                                self.orientationPrefix,
+                                [img stringByReplacingOccurrencesOfString:@"/" withString:@"-"]];
+        
+       // NSLog(@"\nCoverPlate \n\n - File NAme %@ \nwith PAth: %@\n\n\n\n",imgCleaned,dir);
+        NSString *imageName = [[NSBundle mainBundle] 
+                               pathForResource:imgCleaned
+                               ofType:@"png" 
+                               inDirectory:dir];
+        //NSLog(@"\nFacePlate added from - - - Path\n %@\n",imageName);
+        
+        ///////////////////////////////
+        // store internals
+        self.parts = [NSString stringWithFormat:@"%@",imgCleaned];
+        self.price = [[items lastObject] valueForKey:@"price"];
+        
+        /////////////////////////////////////////
+        // Grab the image off disk and load it up
 
-    UIImage *image = [UIImage imageWithContentsOfFile:imageName];
-    UIImageView *nextImage = [[UIImageView alloc] initWithImage:image];
-    nextImage.backgroundColor = [UIColor clearColor];
-    nextImage.contentMode = UIViewContentModeScaleAspectFit;
+        UIImage *image = [UIImage imageWithContentsOfFile:imageName];
+        UIImageView *nextImage = [[UIImageView alloc] initWithImage:image];
+        nextImage.backgroundColor = [UIColor clearColor];
+        nextImage.contentMode = UIViewContentModeScaleAspectFit;
 
-    /////////////////////////////////
-    // calculate size based on interface
-    float iWidth = screenWidth * 0.8;
-    float iHeight = screenHeight * 0.8;
-    float sX = (screenWidth -iWidth)/2.0;
-    float sY = (screenHeight - iHeight)/2.0 - 60;
-    nextImage.frame = CGRectMake(sX, sY, iWidth, iHeight- 100.0);
-    
-    [self.view addSubview:nextImage];
-    [nextImage release];
-
+        /////////////////////////////////
+        // calculate size based on interface
+        float iWidth = screenWidth * 0.8;
+        float iHeight = screenHeight * 0.8;
+        float sX = (screenWidth -iWidth)/2.0;
+        float sY = (screenHeight - iHeight)/2.0 - 60;
+        nextImage.frame = CGRectMake(sX, sY, iWidth, iHeight- 100.0);
+        
+        [self.view addSubview:nextImage];
+        [nextImage release];
+    }
     
     [self addToolBarToView];
     
