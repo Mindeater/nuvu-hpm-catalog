c515df871c9d18dedc8b8c008913c8005d9f6a53
Ashley McCoy <ashley@mindeater.com>
2013-11-22 11:24:35 +1100

    replace the jacky pre iOS7 Alertview with a custom ViewController

---
+++ Catalog2/CollectInfoAlertView.h
@@ -0,0 +1,13 @@
+//
+//  CollectInfoAlertView.h
+//  Catalog2
+//
+//  Created by Ashley McCoy on 21/11/13.
+//  Copyright (c) 2013 Mindeater Web Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectInfoAlertView : UIViewController
+
+@end
---
+++ Catalog2/CollectInfoAlertView.m
@@ -0,0 +1,38 @@
+//
+//  CollectInfoAlertView.m
+//  Catalog2
+//
+//  Created by Ashley McCoy on 21/11/13.
+//  Copyright (c) 2013 Mindeater Web Services. All rights reserved.
+//
+
+#import "CollectInfoAlertView.h"
+
+@interface CollectInfoAlertView ()
+
+@end
+
+@implementation CollectInfoAlertView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
