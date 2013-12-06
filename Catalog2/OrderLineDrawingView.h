b2096212816eec05dee51c4784152c2ed2bf9e44
Ashley McCoy <ashley@mindeater.com>
2013-12-06 15:04:35 +1100

    Custom Drawing class for Orderline view in iOS 7

---
+++ Catalog2/OrderLineDrawingView.h
@@ -0,0 +1,13 @@
+//
+//  OrderLineDrawingView.h
+//  Catalog2
+//
+//  Created by Ashley McCoy on 6/12/13.
+//  Copyright (c) 2013 Mindeater Web Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderLineDrawingView : UIView
+
+@end
---
+++ Catalog2/OrderLineDrawingView.m
@@ -0,0 +1,31 @@
+//
+//  OrderLineDrawingView.m
+//  Catalog2
+//
+//  Created by Ashley McCoy on 6/12/13.
+//  Copyright (c) 2013 Mindeater Web Services. All rights reserved.
+//
+
+#import "OrderLineDrawingView.h"
+
+@implementation OrderLineDrawingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
